a2ec931a33ddf3267e567ceec098c11a5271ad18
Leo Reuter - PC <leo.reuter@hotmail.com>
2024-10-18 12:39:52 -0300

    rota `drotable`

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for, redirect
 from get_request import get_json
 import database
+import time
 
 app = Flask(__name__)
 
@@ -11,14 +12,20 @@ def home():
 @app.route("/get", methods=["GET"])
 def insertDatabase():
     if request.method == "GET":
-        return get_json()
+        get_json()
+        return "teste"
     if request.method != "GET":
         return "Erro de request!"
     
-@app.route("/droptable", methods=["GET"])
+@app.route("/droptable", methods=["GET", "POST"])
 def dropTable():
     if request.method == "GET":
+        return render_template('droptable.html')
+    
+    if request.method == "POST":
         database.dropTable()
-        return "Tabela deletada com sucesso!"
-    if request.method != "GET":
-        return "Erro de request!"
+        return redirect('/')
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
